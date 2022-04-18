2c8217777903f9421f033f8dfffdabaf3ce7d9bb
XuSY <91069819+XuSY021125@users.noreply.github.com>
2022-04-18 12:29:51 +0800

    Update 367.有效的完全平方数.cpp

--- 367.有效的完全平方数.cpp
+++ 367.有效的完全平方数.cpp
@@ -1,17 +1,14 @@
-int mySqrt(int x){
-    {
-        if(x == 1)
-            return 1;
-        int min = 0;
-        int max = x;
-        while(max-min>1)
-        {
-            int m = (max+min)/2;
-            if(x/m<m)
-                max = m;
-            else
-                min = m;
-        }
-        return min;
+bool isPerfectSquare(int num){
+int low = 1, high = num, mid;
+
+    while(low <= high){
+        mid = low + (high - low) / 2;
+        if(num / mid == mid && num % mid == 0)  
+            return true;
+        else if(num / mid < mid)
+            high = mid-1;
+        else
+            low = mid+1;
     }
+    return false;                              
 }
